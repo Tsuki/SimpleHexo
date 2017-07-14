3a66bf650e6b72f0a6d349f148a68b88ec9f03dd
Tsuki <i@sukitsuki.com>
2017-07-14 09:43:28 +0800

    config
    Signed-off-by: Tsuki <i@sukitsuki.com>

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,18 @@
  */
 import * as core from './core'
 import {debug, info, warn, error} from './logger'
-import * as config from './simConfig'
+import {readConfig, writeConfig, configExists} from './SimConfig'
 import * as program from 'commander';
 program
     .version('0.1.0')
     .command('init')
     .description('Initialize or update simhex config')
     .action(() => {
-        if (config.configExists()) {
-            info('config existed')
+        if (configExists()) {
+            info('config existed');
         } else {
-            info('config existed2');
-            config.write();
+            debug('init config');
+            writeConfig();
         }
     });
 
@@ -22,6 +22,8 @@ program
     .command('sync')
     .description('Initialize or update simhex config')
     .action(() => {
+        let config = readConfig();
+        debug('config:%O', config);
 
     });
 
